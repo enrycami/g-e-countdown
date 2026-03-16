64b3373b7fa20948a90cfbbfa8e01e4e70183b08
enrycami <19608008+enrycami@users.noreply.github.com>
2026-03-16 07:25:05 +0000

    Reworked sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
 const CACHE_NAME = 'manila-v1';
-const ASSETS = ['./index.html'];
+const ASSETS = [
+  './', 
+  './index.html',
+  './manifest.json',
+  './app.js',
+  './sw.js',
+  './images/icon16px.png',
+  './images/icon32px.png',
+  './images/icon128px.png',
+  './images/icon512px.png',
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+    ))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
